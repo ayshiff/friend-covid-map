bb19dcf7ddc159da682e23b7589e4bf8ad4198b1
Rémi DOREAU <remi.d45@gmail.com>
2020-05-20 04:06:14 +0200

    feat(chore): init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MapComponent } from "./components/templates/map";
+import "@progress/kendo-theme-default/dist/all.css";
+import { DashboardComponent } from "./components/templates/dashboard";
+import DrawerRouterContainer from "./navigation/DrawerRouterContainer";
+
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <HashRouter>
+        <DrawerRouterContainer>
+          <Switch>
+            <Route exact={true} path="/" component={MapComponent} />
+            <Route
+              exact={true}
+              path="/dashboard"
+              component={DashboardComponent}
+            />{" "}
+            />
+          </Switch>
+        </DrawerRouterContainer>
+      </HashRouter>
+      <style>
+        {`my-app {
+            padding: 0;
+        }
+        .k-drawer-container {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+        }
+        .k-drawer-item {
+            user-select: none;
+        }
+        .k-icon {
+            font-size: 20px;
+        }
+        .page ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+        .page li {
+            font-size: 1.2em;
+            background: 0 0;
+            border-radius: 0;
+            border-width: 0 0 1px;
+            border-color: rgba(33, 37, 41, 0.125);
+            border-style: solid;
+            line-height: 1.5em;
+            padding: 1.09em .84em 1.23em .84em;
+        }
+        .page li:last-child {
+            border: 0;
+        }`}
+      </style>
+    </React.Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { PersistGate } from "redux-persist/integration/react";
+
+import { Provider } from "react-redux";
+import { store, persistor } from "./config/store";
+import "./index.css";
+
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
